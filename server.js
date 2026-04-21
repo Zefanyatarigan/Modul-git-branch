a729632dc93d81165553fb12f858f77cba067c10
Zefanya Tarigan <zefanyatarigan98@gmailo.com>
2026-04-21 16:28:54 +0700

    Merapikan server.js setelah merge conflict

--- server.js
+++ server.js
@@ -49,10 +49,3 @@ app.get('/api/status', (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server berjalan di http://localhost:${process.env.PORT}`);
 });
-<<<<<<< HEAD
-
-app.get('/api/status', (req, res) => {
-  res.json({ message: 'Backend berjalan dengan baik' });
-});
-=======
->>>>>>> origin/main
